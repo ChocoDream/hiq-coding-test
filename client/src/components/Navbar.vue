e3f8ba8bb064df9a3d1f8d74bc8ed6b3be1dce60
DanielHansson <dh99@live.se>
2021-10-14 02:53:01 +0200

    Removing default code, added navbar
    Removed boilerplate default code that came when generating new project with vue create.  Implemented basic navbar.

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <header id="header">
+    <h1 id="header-logo">textProcessorCO</h1>
+    <h1 id="header-developer">by Daniel Hansson</h1>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+#header{
+  display: flex;
+  justify-content: space-around;
+  align-items: flex-start;
+  background-color: var(--primary);
+  text-align: center;
+}
+</style>
